0e57a03c75bc64cbb0912f6caf2b47d748c0d7e6
edson <mbgraficarn@gmail.com>
2023-05-22 20:59:41 -0300

    aula condicionais parte 1

---
+++ script.js
@@ -0,0 +1,43 @@
+//const booleano1 = true
+//const booleano2 = false
+//const booleano3 = 2
+
+//if(booleano1){
+//    alert("Booleano 1 e True")
+//} else{
+//    alert("Boleano 1 False")
+
+//}
+
+//if(booleano1===booleano2){
+ //   alert("Gol do Booleano")
+//} else if(booleano2===booleano3){
+//    alert("Gol do Booleano")
+//} else if(booleano1===booleano3){
+//    alert("Gol do Booleano")
+//} else {
+//    alert("Boleano Impedido")
+//}
+
+//const idade = 13
+
+//if (idade >= 18 && idade < 70) {
+//    alert("Pode Votar")
+//}else if(idade === 16 || idade === 17){
+//    alert("Pode votar com autorização")
+//}else if(idade >= 70){
+//    alert("Pode votar, mas não he obrigadotorio")
+//}else{
+//    alert("Não pode votar")
+//}
+
+const media = 7
+if (media >= 5) {
+    console.log("Aprovada");
+}else if(media >=3){
+    console.log("Recuperação");
+}else if(media < 3){
+    console.log("Aluno Reprovado");
+}else {
+    console.log("Dado Invalido");
+}
